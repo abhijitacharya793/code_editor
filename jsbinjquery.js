3d3dfb52e8941fae31c48c63d9facdedcfd180b3
abhijit acharya <abhijitacharya793@gmail.com>
2021-04-10 21:15:59 +0530

    modified to consider ctrl+s

--- jsbinjquery.js
+++ jsbinjquery.js
@@ -17,41 +17,60 @@ $(document).ready(function () {
     $(activeDiv).filter(".selected").css("width", width);
   });
 
+  document.addEventListener(
+    "keydown",
+    function (e) {
+      if (
+        (window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) &&
+        e.keyCode == 83
+      ) {
+        e.preventDefault();
+        runCode();
+      }
+    },
+    false
+  );
+
   $("#run").click(function () {
-    const getGeneratedPageURL = ({ html, css, js }) => {
-      const getBlobURL = (code, type) => {
-        const blob = new Blob([code], { type });
-        return URL.createObjectURL(blob);
-      };
-
-      const cssURL = getBlobURL(css, "text/css");
-      const jsURL = getBlobURL(js, "text/javascript");
-
-      const source = `
-          <html>
-            <head>
-              ${
-                css &&
-                `<link rel="stylesheet" type="text/css" href="${cssURL}" />`
-              }
-              ${js && `<script src="${jsURL}"></script>`}
-            </head>
-            <body>
-              ${html || ""}
-            </body>
-          </html>
-        `;
-
-      return getBlobURL(source, "text/html");
-    };
-
-    const url = getGeneratedPageURL({
-      html: $("#htmlContent").val() + "",
-      css: $("#cssContent").val() + "",
-      js: $("#jsContent").val() + "",
-    });
-
-    const iframe = document.querySelector(".iframe");
-    iframe.src = url;
+    runCode();
   });
 });
+
+function runCode() {
+  console.log("Run clicked");
+  const getGeneratedPageURL = ({ html, css, js }) => {
+    const getBlobURL = (code, type) => {
+      const blob = new Blob([code], { type });
+      return URL.createObjectURL(blob);
+    };
+
+    const cssURL = getBlobURL(css, "text/css");
+    const jsURL = getBlobURL(js, "text/javascript");
+
+    const source = `
+        <html>
+          <head>
+            ${
+              css &&
+              `<link rel="stylesheet" type="text/css" href="${cssURL}" />`
+            }
+            ${js && `<script src="${jsURL}"></script>`}
+          </head>
+          <body>
+            ${html || ""}
+          </body>
+        </html>
+      `;
+
+    return getBlobURL(source, "text/html");
+  };
+
+  const url = getGeneratedPageURL({
+    html: $("#htmlContent").val() + "",
+    css: $("#cssContent").val() + "",
+    js: $("#jsContent").val() + "",
+  });
+
+  const iframe = document.querySelector(".iframe");
+  iframe.src = url;
+}
